e408362e3ea1308c48248b250e8ce428f4ca1a1d
nikolay dementiev <nikolay@medisafe.com>
2025-01-14 11:48:08 +0100

    ↣ add DMErrorHandlingSPMExample

---
+++ Examples/DMErrorHandlingSPMExample/DMErrorHandlingSPMExample/DMErrorHandlingSPMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DMErrorHandlingSPMExampleApp.swift
+//  DMErrorHandlingSPMExample
+//
+//  Created by Nikolay Dementiev on 14.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DMErrorHandlingSPMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
