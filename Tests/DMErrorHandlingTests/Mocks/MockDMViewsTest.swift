16b498fff9e900c3abd2417da323cabd29c7acfd
nikolay dementiev <nikolay@medisafe.com>
2025-02-17 16:33:22 +0100

    add test for DMLoadingViewProvider

---
+++ Tests/DMErrorHandlingTests/Mocks/MockDMViewsTest.swift
@@ -0,0 +1,26 @@
+//
+//  MockDMSuccessViewTest.swift
+//  DMErrorHandling
+//
+//  Created by Nikolay Dementiev on 17.02.2025.
+//
+
+import SwiftUI
+
+struct MockDMSuccessViewTest: View {
+    var body: some View {
+        Text("Success View Test")
+    }
+}
+
+struct MockDMLoadingViewTest: View {
+    var body: some View {
+        Text("Loading View Test")
+    }
+}
+
+struct MockDMErrorViewTest: View {
+    var body: some View {
+        Text("Error View Test")
+    }
+}
