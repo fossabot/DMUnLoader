208e9e43571a17620da0674e1c4903b915efe4d4
Mykola Dementiev <nikolas.dementiev@gmail.com>
2025-09-05 15:11:05 +0200

    switch to SPM for test project

--- Examples/DMUnLoaderPodSPMExample/Pods/Target Support Files/DMAction/DMAction-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DMAction : NSObject
-@end
-@implementation PodsDummy_DMAction
-@end
--- Examples/DMUnLoaderPodSPMExample/Pods/Target Support Files/DMUnLoader/DMUnLoader-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DMUnLoader : NSObject
-@end
-@implementation PodsDummy_DMUnLoader
-@end
